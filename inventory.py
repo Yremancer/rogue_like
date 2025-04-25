fb07cd585d9993fbb13d54c5c1d86caf180bdbb7
bebrakod <anton.chusovikin33@gmail.com>
2025-04-25 23:21:22 +1000

    Add files via upload

--- inventory.py
+++ inventory.py
@@ -2,23 +2,24 @@ class Item:
     def __init__(self, name):
         self.name = name
 
-class Gun_part(Item):
+
+class Gun_parts(Item):
     view: str = "*"
     def __init__(self, max_parts):
         self.max_parts = max_parts
-        gun_parts_count =+ 1
-        if gun_parts_count == max_parts:
-            Inventory.items.append("gun")
+    
+
+class Key(Item):
+    pass
+
 
 class Inventory:
     def __init__(self, items=[]):
         self.items = items
 
-    
     def add_item(self, item):
             self.items.append(item)
-
-gun_part1 = Gun_part(3)
-inventory = Inventory()
-inventory.add_item (gun_part1)
-print(inventory.items)
+            if isinstance(item, Gun_parts):
+                gun_parts_count =+ 1
+                if gun_parts_count == self.max_parts:
+                    Inventory.items.append("gun")
